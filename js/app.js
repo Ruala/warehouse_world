9fa6ad808791efefccbea783ccb1cca2497dd8b0
Dmitrij Lucenko <333333y@gmail.com>
2018-08-13 19:40:19 +0530

    Echarts basics

--- js/app.js
+++ js/app.js
@@ -21,7 +21,7 @@ $(document).ready(function () {
             filterGrid($activeButton, $controlButtons, $items, true);
 
 
-            $controlContainer.on('click', 'li', function(evt) {
+            $controlContainer.on('click', 'li', function (evt) {
                 const $button = $(this);
                 evt.preventDefault();
 
@@ -44,5 +44,101 @@ $(document).ready(function () {
             const $visible = $items.filter(filterValue);
             $visible.fadeIn(200);
         }
+    })();
+
+    /*custom toggler class*/
+    (function () {
+        const $togglers = $('[uk-toggle]');
+
+        $togglers.each(function () {
+            const $toggler = $(this);
+            const selector = $toggler.attr('href') && $toggler.attr('href').length > 2 ?
+                $toggler.attr('href') :
+                getSelector($toggler.attr('uk-toggle'));
+            const $target = $(selector);
+
+            $target.on({
+                'beforeshow': function () {
+                    $toggler.addClass('uk-active');
+                },
+                'hidden': function () {
+                    $toggler.removeClass('uk-active');
+                }
+            });
+        });
+
+        function getSelector(str) {
+            const start = 'target: ';
+            const end = ';';
+
+            if (!~str.indexOf(start)) return null;
+
+            const startPos = str.indexOf(start) + start.length;
+            let endPos = ~str.indexOf(end, startPos) ? str.indexOf(end, startPos) : undefined;
+
+            return str.slice(startPos, endPos);
+        }
+    })();
+
+    /*sort*/
+    (function () {
+
+    })();
+
+    /*charts*/
+    (function () {
+        const $charts = $('[bar-chart]');
+
+        $charts.each(function () {
+            const el = this;
+            const $el = $(this);
+            const $accordion = $el.closest('[uk-accordion]');
+            const chart = echarts.init(el);
+            const option = el.getAttribute('bar-chart').length > 2 ?
+                JSON.stringify(el.getAttribute('bar-chart')) :
+                {
+                    tooltip: {
+                        trigger: 'axis',
+                        // axisPointer: {
+                        //     type: 'shadow',
+                        // }
+                    },
+                    toolbox: {
+                        show: true,
+                        orient: 'vertical',
+                        left: 'right',
+                        top: 'center',
+                        feature: {
+                            mark: { show: true },
+                            dataView: { show: true, readOnly: false },
+                            magicType: { show: true, type: ['line', 'bar', 'stack', 'tiled'] },
+                            restore: { show: true },
+                            saveAsImage: { show: true },
+                        },
+                    },
+                    xAxis: {
+                        type: 'category',
+                        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+                    },
+                    yAxis: {
+                        type: 'value'
+                    },
+                    series: [{
+                        data: [820, 932, 901, 934, 1290, 1330, 1320],
+                        type: 'line'
+                    }]
+                };
+
+            // use configuration item and data specified to show chart
+            chart.setOption(option);
+
+            $accordion.on('show shown', function () {
+                if (!$el.closest('.uk-open').length) return;
+
+                chart.resize();
+            });
+
+            $(window).on('resize', chart.resize.bind(chart));
+        });
     })();
 });
