f820a9ff19d145b0ae772c501d3c305e3ae04fc0
Lutsman_win <333333y@gmail.com>
2018-08-14 16:19:52 +0530

    chat togglers

--- js/app.js
+++ js/app.js
@@ -49,7 +49,7 @@ $(document).ready(function () {
     /*element togglers premade*/
     (function () {
         /*toggler simple*/
-        (function() {
+        (function () {
             const $toggler = $('.js__et');
             const options = {};
 
@@ -57,7 +57,7 @@ $(document).ready(function () {
         })();
 
         /*toggler no animate*/
-        (function() {
+        (function () {
             const $toggler = $('.js__et-na');
             const options = {
                 animation: 'none'
@@ -67,7 +67,7 @@ $(document).ready(function () {
         })();
 
         /*toggler fade*/
-        (function() {
+        (function () {
             const $toggler = $('.js__et-fa');
             const options = {
                 animation: 'fade'
@@ -77,7 +77,7 @@ $(document).ready(function () {
         })();
 
         /*toggler slide*/
-        (function() {
+        (function () {
             const $toggler = $('.js__et-sla');
             const options = {
                 animation: 'slide'
@@ -87,7 +87,7 @@ $(document).ready(function () {
         })();
 
         /*toggler simple parent lvl 1*/
-        (function() {
+        (function () {
             const $toggler = $('.js__et-p1');
             const options = {
                 getTarget: function ($btn) {
@@ -99,7 +99,7 @@ $(document).ready(function () {
         })();
 
         /*toggler no animate  parent lvl 1*/
-        (function() {
+        (function () {
             const $toggler = $('.js__et-na-p1');
             const options = {
                 getTarget: function ($btn) {
@@ -112,7 +112,7 @@ $(document).ready(function () {
         })();
 
         /*toggler fade  parent lvl 1*/
-        (function() {
+        (function () {
             const $toggler = $('.js__et-fa-p1');
             const options = {
                 getTarget: function ($btn) {
@@ -125,7 +125,7 @@ $(document).ready(function () {
         })();
 
         /*toggler slide  parent lvl 1*/
-        (function() {
+        (function () {
             const $toggler = $('.js__et-sla-p1');
             const options = {
                 getTarget: function ($btn) {
@@ -156,6 +156,89 @@ $(document).ready(function () {
 
     /*chat togglers*/
     (function () {
+        const togglerGroup = {};
+
+        class Chattoggler {
+            constructor(options) {
+                this.toggler = options.toggler;
+                this.isActive = false;
+                this.classNames = {
+                    active: 'et-active',
+                    closeBtn: 'tm-chat-settings-target-close',
+                    counter: 'tm-chat-settings-counter',
+                };
+
+                this.init();
+            }
+
+            init() {
+                this.bindElements();
+                this.bindHandlers();
+                this.attachHandlers();
+            }
+
+            bindElements() {
+                this.$toggler = $(this.toggler);
+                this.groupname = this.$toggler.attr('chat-toggler');
+                this.$target = $(this.groupname);
+                this.$closeBtn = this.$target.find('.' + this.classNames.closeBtn);
+                // implement counter calculation
+                this.$counter = this.$target.find('.' + this.classNames.counter);
+            }
+
+            bindHandlers() {
+                this._toggle = this.toggle.bind(this);
+                this._close = this.close.bind(this);
+            }
+
+            attachHandlers() {
+                this.$toggler.on('click', this._toggle);
+                this.$closeBtn.on('click', this._close);
+            }
+
+            toggle() {
+                if (this.isActive) {
+                    this.toggleOff();
+                } else {
+                    this.toggleOn();
+                }
+            }
+
+            close(evt) {
+                evt.preventDefault();
+                this.toggleOff();
+            }
+
+            toggleOff() {
+                togglerGroup[this.groupname] = togglerGroup[this.groupname] > 0 ? togglerGroup[this.groupname] - 1 : 0;
+                this.$toggler.removeClass(this.classNames.active);
+                this.isActive = false;
+                if (togglerGroup[this.groupname] === 0) {
+                    this.hideTarget();
+                }
+            }
+
+            toggleOn() {
+                togglerGroup[this.groupname] = togglerGroup[this.groupname] ? togglerGroup[this.groupname] + 1 : 1;
+                this.$toggler.addClass(this.classNames.active);
+                this.isActive = true;
+                this.showTarget();
+            }
+
+            hideTarget() {
+                this.$target.fadeOut();
+            }
+
+            showTarget() {
+                this.$target.fadeIn();
+            }
+        }
+
+        const $togglers = $('[chat-toggler]');
+
+        $togglers.each(function () {
+            new Chattoggler({toggler: this});
+        })
 
     })();
 
@@ -208,8 +291,8 @@ $(document).ready(function () {
             const height = size.bottom - size.top;
 
             return {
-                x: size.left + width/2,
-                y: size.top + height/2,
+                x: size.left + width / 2,
+                y: size.top + height / 2,
             };
         }
 
